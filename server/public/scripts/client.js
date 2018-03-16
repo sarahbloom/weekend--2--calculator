4a60daf1156824bf512a7929f7c49dc4b9ba1e99
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-16 16:09:05 -0500

    get completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,16 +5,36 @@ function readyNow() {
     $('.calculate').on('click', submitEquation);
 }
 
+//GET
+function getallEquations() {
+    $.ajax({
+        type: "GET", 
+        url: "/equation"
+    }).done(function(response){
+        console.log('done!');
+        // appendToDom(response); // response is the array
+    })
+}
+
+//POST
 function submitEquation(){
     let firstDigit = $('#firstNumber').val();
     let secondDigit = $('#secondNumber').val();
     let numbersSubmitted = { firstDigit: firstDigit, secondDigit: secondDigit}
-    console.log(firstDigit, secondDigit);
+    console.log(numbersSubmitted);
     $.ajax({
         type: "POST",
         data: numbersSubmitted,
         url: "/equation"
     }).done(function (response) {
-    console.log('success!');
+        console.log('success!');
+        getallEquations() 
     })
 }
+
+// function appendToDom(){
+//     $('#history').clear();
+//     for (let equation of equationHistory){
+//         console.log('Equation: ', equation);
+//     }
+// }
--- server/server.js
+++ server/server.js
@@ -2,17 +2,25 @@ let express = require('express');
 let app = express();
 const PORT = process.env.PORT || 5000;
 let bodyParser = require('body-parser');
+const equationHistory = []; //array
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('server/public'));
 
-app.post('/equation', (req, res) =>{
-    console.log(req.body);
+//GET
+app.get('/equation', (req, res) => {
+    res.send(equationHistory);
+})
+
+//POST
+app.post('/equation', (req, res) => {
+    console.log(equationHistory);
     let numbersSubmitted = req.body;
+    equationHistory.push(numbersSubmitted);
     res.sendStatus(200);
 })
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log("server is running on port: ", PORT);
 })
