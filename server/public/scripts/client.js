c4ad40b8ae07be71f916028dfb2ee00683b76812
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-16 15:50:22 -0500

    POST completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,19 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jQuery loaded');
+    $('.calculate').on('click', submitEquation);
+}
+
+function submitEquation(){
+    let firstDigit = $('#firstNumber').val();
+    let secondDigit = $('#secondNumber').val();
+    let numbersSubmitted = { firstDigit: firstDigit, secondDigit: secondDigit}
+    console.log(firstDigit, secondDigit);
+    $.ajax({
+        type: "POST",
+        data: numbersSubmitted,
+        url: "/equation"
+    }).done(function (response) {
+    console.log('success!');
+    })
 }
--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('server/public'));
 
+app.post('/equation', (req, res) =>{
+    console.log(req.body);
+    let numbersSubmitted = req.body;
+    res.sendStatus(200);
+})
+
 app.listen(PORT, () =>{
     console.log("server is running on port: ", PORT);
 })
